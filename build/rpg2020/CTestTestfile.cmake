f3f9848baebdc76e86aab44dc17b1d4fae1735a8
Geek2000IRX <11810502@mail.sustech.edu.cn>
2020-05-23 03:32:36 +0800

    Init Project

---
+++ build/rpg2020/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/geek2000/catkin_ws/src/rpg2020
+# Build directory: /home/geek2000/catkin_ws/build/rpg2020
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
